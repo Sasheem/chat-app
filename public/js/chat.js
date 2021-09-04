6416f9ffdb3ccda352286c4226745b2072def8bb
Sasheem Snell <sasheem.snell@gmail.com>
2021-09-04 12:26:47 -0400

    send location coords to server

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,3 +12,15 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message)
 })
+
+// listen for when user clicks submit button
+document.querySelector('#share-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { longitude, latitude } = position.coords
+        socket.emit('sendLocation', { longitude, latitude })
+    })
+})

48f20a1b11ae938e2cba9fb1e6aaf476c1d0fa69
Sasheem Snell <sasheem.snell@gmail.com>
2021-09-04 12:01:05 -0400

    send message from client to server

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,14 @@
-io()
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+// listen for when the user submtis form
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    // send message from form to server
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message)
+})
